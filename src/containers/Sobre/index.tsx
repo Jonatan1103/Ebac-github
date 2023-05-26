81666e2db3879e14c317b7372adb4d38890d5165
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2023-05-26 10:41:08 -0300

    secao sobre finalizada

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,13 +1,19 @@
 import Titulo from "../../components/Titulo"
 import Paragrafo from "../../components/Paragrafo"
+import { GithubSecao } from "./styles"
 
 const Sobre = () => {
   return (
     <section>
       <Titulo fontSize={16}>Titulo</Titulo>
-      <Paragrafo tipo="secundario">
+      <Paragrafo tipo="principal">
         Lorem isum dolor sit amet consectetur adipisicing elit. Debitis, ipsum totam! Delectus consequuntur mollitia, possimus est at illo incidunt in quis quod saepe perferendis nam explicabo reprehenderit minus facilis doloremque?
       </Paragrafo>
+
+      <GithubSecao>
+        <img src="https://github-readme-stats.vercel.app/api?username=Jonatan1103&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Jonatan1103&layout=compact&langs_count=7&theme=dracula"/>
+      </GithubSecao>
     </section>
   )
 }
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const GithubSecao = styled.div`
+  margin: 32px 0 64px 0;
+
+  img {
+    height: 157px;
+    
+    @media (max-width: 768px) {
+      height: 200px;
+    }
+  }
+`
